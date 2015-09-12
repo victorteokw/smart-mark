28c23265f9cf4dd00ee6d21072926b0bac42a025
Steve Purcell <steve@sanityinc.com>
2015-09-12 13:50:28 +1200

    Dynamically create all mark-* function advice from list

--- smart-mark.el
+++ smart-mark.el
@@ -27,49 +27,31 @@
 (defvar smart-mark-point-before-mark nil
   "Cursor position before mark.")
 
-(defvar smart-mark-mark-functions
-  '(mark-page mark-paragraph mark-whole-buffer mark-sexp mark-defun mark-word)
-  "Functions with marking behavior.")
+(eval-and-compile
+  (defconst smart-mark-mark-functions
+    '(mark-page mark-paragraph mark-whole-buffer mark-sexp mark-defun mark-word)
+    "Functions with marking behavior."))
 
-(defun smart-mark-set-advices ()
-  (defadvice mark-page (before smart-mark-set-restore-before-mark activate)
-    (setq smart-mark-point-before-mark (point)))
-  (defadvice mark-paragraph (before smart-mark-set-restore-before-mark activate)
-    (setq smart-mark-point-before-mark (point)))
-  (defadvice mark-whole-buffer (before smart-mark-set-restore-before-mark activate)
-    (setq smart-mark-point-before-mark (point)))
-  (defadvice mark-sexp (before smart-mark-set-restore-before-mark activate)
-    (setq smart-mark-point-before-mark (point)))
-  (defadvice mark-defun (before smart-mark-set-restore-before-mark activate)
-    (setq smart-mark-point-before-mark (point)))
-  (defadvice mark-word (before smart-mark-set-restore-before-mark activate)
-    (setq smart-mark-point-before-mark (point)))
-  (defadvice keyboard-quit (before smart-mark-restore-cursor-cg-mark activate)
-    (when (memq last-command smart-mark-mark-functions)
-      (smart-mark-restore-cursor))))
+(defmacro smart-mark-advise-all ()
+  "Advise all `smart-mark-mark-functions' so that point is initially saved."
+  `(progn
+     ,@(mapcar (lambda (f)
+                 `(defadvice ,f (before smart-mark-set-restore-before-mark activate)
+                    "Save point to `smart-mark-point-before-mark' before this function runs."
+                    (setq smart-mark-point-before-mark (point))))
+               smart-mark-mark-functions)))
 
-(defun smart-mark-remove-advices ()
-  (advice-remove 'mark-page #'smart-mark-set-restore-before-mark)
-  (advice-remove 'mark-paragraph #'smart-mark-set-restore-before-mark)
-  (advice-remove 'mark-whole-buffer #'smart-mark-set-restore-before-mark)
-  (advice-remove 'mark-sexp #'smart-mark-set-restore-before-mark)
-  (advice-remove 'mark-defun #'smart-mark-set-restore-before-mark)
-  (advice-remove 'mark-word #'smart-mark-set-restore-before-mark)
-  (advice-remove 'keyboard-quit #'smart-mark-restore-cursor-cg-mark))
+(smart-mark-advise-all)
+
+(defadvice keyboard-quit (before smart-mark-restore-cursor-cg-mark activate)
+  (when (memq last-command smart-mark-mark-functions)
+    (smart-mark-restore-cursor)))
 
 (defun smart-mark-restore-cursor ()
   "Restore cursor position saved just before mark."
   (when smart-mark-point-before-mark
     (goto-char smart-mark-point-before-mark)
     (setq smart-mark-point-before-mark nil)))
-
-;;;###autoload
-(define-minor-mode smart-mark-mode
-  "Mode for easy expand line when expand line is activated."
-  :global t
-  (if smart-mark-mode
-      (smart-mark-set-advices)
-    (smart-mark-remove-advices)))
 
 (provide 'smart-mark)
 ;;; smart-mark.el ends here
