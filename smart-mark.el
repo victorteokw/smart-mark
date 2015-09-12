d179cdc3f53001a5ce99d5095f493cdf3a792567
Zhang Kai Yu <yeannylam@gmail.com>
2015-09-12 10:10:14 +0800

    Add documentation

--- smart-mark.el
+++ smart-mark.el
@@ -20,7 +20,9 @@
 
 ;;; Commentary:
 
-;; (require 'smart-mark)
+;; smart-mark-mode is a global minor mode.
+;; When it is active and you press C-g after a mark function,
+;; the cursor restores to its initial location.
 
 ;;; Code:
 
